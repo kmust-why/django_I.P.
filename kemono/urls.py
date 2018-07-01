106cc03aee6a8edc6a40fc7fdeaea71bcbe08e74
github S-ANAI <s153309anai@gmail.com>
2018-07-01 18:37:28 +0900

    FirstCommit

--- kemono/urls.py
+++ kemono/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('image.urls', namespace='image')),
+    url(r'^', include('image.urls')),
 ]
 
 if settings.DEBUG:
